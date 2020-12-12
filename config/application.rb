87bf7a2831cc90e1c744c7f8a23fe6a034ebfd79
nickreznichuk <nickreznichuk@gmail.com>
2020-12-12 20:56:20 +0200

    change some

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module BrowsShedule
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.time_zone = 'Kyiv'
-    config.hosts << "cbcaa5ae60a7.ngrok.io"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
