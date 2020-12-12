f317afb0df70df5256631b975506cc942baf59c4
nickreznichuk <nickreznichuk@gmail.com>
2020-12-12 21:56:23 +0200

    deploy fixes

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module BrowsShedule
     config.load_defaults 6.0
     config.time_zone = 'Kyiv'
     config.assets.compile = true
+    config.public_file_server.enabled = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
